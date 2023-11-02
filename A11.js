9fcee8e30fc8c57fc8d35a2db695510447363e33
Santosh Chulakimath <72141660+chulakimath@users.noreply.github.com>
2023-11-02 21:46:51 +0530

    Update A11.js

--- A11.js
+++ A11.js
@@ -1,38 +1,49 @@
-function show_image(){
-const apiUrl = 'https://api.waifu.im/search';  // Replace with the actual API endpoint URL
-const params = {
-  included_tags: 'maid',
-  height: '>=2000'
-};
-fetch(apiUrl,params)
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error('Request failed with status code: ' + response.status);
-    }
-  })
-  .then(data => {
+function show_image() {
+  const apiUrl = 'https://api.waifu.im/search';  // Replace with the actual API endpoint URL
+  const params = {
+    included_tags: 'maid',
+    height: '>=2000'
+  };
+  fetch(apiUrl, params)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Request failed with status code: ' + response.status);
+      }
+    })
+    .then(data => {
 
-    let downloadUrl=data.images[0].url;
-    console.log(downloadUrl);
-    displayImage(downloadUrl);
-  })
-  .catch(error => {
-    console.error('An error occurred:', error.message);
-  });
+      let downloadUrl = data.images[0].url;
+      console.log(downloadUrl);
+      displayImage(downloadUrl);
+    })
+    .catch(error => {
+      console.error('An error occurred:', error.message);
+    });
 }
 show_image()
 function displayImage(url) {
   let doom = document.querySelector(".images");
   let imgTag = document.createElement('img')
   imgTag.src = url;
-  imgTag.id='iimmgg'
+  imgTag.id = 'iimmgg';
+  // imgTag.loading = 'lazy';
   imgTag.alt = '❤️wifu Image❤️';
-  let oldImg=document.getElementById('iimmgg');
-  doom.replaceChild(imgTag,oldImg);
-  // doom.innerHTML=imgTag;
+  let oldImg = document.getElementById('iimmgg');
+  doom.replaceChild(imgTag, oldImg);
+
+  // to ddownload image
+  document.getElementById('downloadTheImage').addEventListener('click', () => {
+    let anc = document.createElement('a');
+    anc.href = url;
+    anc.download = `image_${url}`;
+    document.body.appendChild(anc);
+    anc.click();
+    document.body.removeChild(anc);
+  })
+
 }
-document.getElementById('newImg').addEventListener('click',function(){
+document.getElementById('newImg').addEventListener('click', function () {
   show_image()
 })
